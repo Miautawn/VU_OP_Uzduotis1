4cb6566e3cc0bbcf1e54d4a393107c2d22f4844b
miautawn <miautawn@gmail.com>
2021-02-10 20:54:23 +0200

    added functionality for a dynamic greeting depending on the gender

--- main.cpp
+++ main.cpp
@@ -6,25 +6,31 @@ using std::cin;
 using std::endl;
 using std::string;
 
+//
+string greeting_check(string name)
+{
+  return (name[name.length() - 1] == 'a') ? "Sveika, " : "Sveikas, ";
+}
+
 int main() {
 
   // vardo įvedimas
-  string greeting = "Sveikas, ";
   string name;
   cout<<"Enter your name: ";
   cin>>name;
+
+  string greeting = greeting_check(name);
 
   //eilučių ilgis: *_ + pasisveikinimas + vardas + ! + _*
   int row_length = 5 + greeting.length() + name.length();
 
   //veiksmai su eilutėmis
   string row_1 (row_length, '*');
-
   //padarome, kad antroje ir ketvirtoje eilutėse būtų po '*' iš galų 
   string row_2 (row_length, ' ');
   row_2[0] = '*';
   row_2[row_length-1] = '*';
-
+  //padarome, kad antroje ir ketvirtoje eilutėse būtų po '*' iš galų 
   string row_3 = "* " + greeting + name + "!" + " *";
   string row_4 = row_2;
   string row_5 = row_1; 
